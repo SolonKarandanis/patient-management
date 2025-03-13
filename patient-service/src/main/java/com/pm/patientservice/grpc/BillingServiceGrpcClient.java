667b8396da83581c9e2b14886c57dd94dcc475b8
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-03-13 13:14:26 +0200

    Added proto items

---
+++ patient-service/src/main/java/com/pm/patientservice/grpc/BillingServiceGrpcClient.java
@@ -0,0 +1,6 @@
+package com.pm.patientservice.grpc;
+
+public interface BillingServiceGrpcClient {
+//    public BillingResponse createBillingAccount(String patientId, String name,
+//                                                String email);
+}

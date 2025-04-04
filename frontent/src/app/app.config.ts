993339a5ee2b749fb3c7b7cf11fb0d8f9d13e2ce
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-04-04 13:41:04 +0300

    Added cinfiguration

--- frontent/src/app/app.config.ts
+++ frontent/src/app/app.config.ts
@@ -1,8 +1,49 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import { MessageService } from 'primeng/api';
+import Aura from '@primeng/themes/aura';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader'
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
 
 import { routes } from './app.routes';
+import {HttpBackend, HttpClient, provideHttpClient} from "@angular/common/http";
+import {firstValueFrom} from "rxjs";
+
+export const provideTranslation = () => ({
+  defaultLanguage: 'en',
+  loader: {
+    provide: TranslateLoader,
+    useFactory: createTranslateLoader,
+    deps: [HttpBackend],
+  },
+});
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+      MessageService,
+      provideZoneChangeDetection({ eventCoalescing: true }),
+      provideRouter(routes),
+      provideAnimationsAsync(),
+      providePrimeNG({
+        theme: {
+          preset: Aura
+        }
+      }),
+      provideHttpClient(),
+      importProvidersFrom(TranslateModule.forRoot(provideTranslation())),
+  ]
 };
+
+
+export function createTranslateLoader(httpHandler: HttpBackend): TranslateHttpLoader {
+  return  new  TranslateHttpLoader(new HttpClient(httpHandler), './assets/i18n/', '.json');
+}
+
+export function appInitializerFactory(translate: TranslateService) {
+  return (): Promise<any> => {
+    translate.setDefaultLang('en');
+    return firstValueFrom(translate.use('en'));
+  };
+}

6d6bada2a8609df9de413d41e659fd7b07c61546
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-04-30 08:41:45 +0300

    Implementing Auth pages

--- frontent/src/app/auth/auth.component.ts
+++ frontent/src/app/auth/auth.component.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
     <div class="auth-container">
       <main>
         <section class="relative w-full h-full py-40 min-h-screen">
+          <div
+            class="absolute top-0 w-full h-full bg-blueGray-800 bg-no-repeat bg-full"
+            style="background-image: url('../../assets/img/register_bg_2.png')"
+          ></div>
           <router-outlet></router-outlet>
           <app-footer-small [absolute]="true"></app-footer-small>
         </section>

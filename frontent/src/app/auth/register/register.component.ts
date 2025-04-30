a577599dfc33c40ff837fee871ceff3c031fec50
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-04-30 08:00:11 +0300

    Implementing Auth functionality

---
+++ frontent/src/app/auth/register/register.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  imports: [],
+  template: `
+    <p>
+      register works!
+    </p>
+  `,
+  styleUrl: './register.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RegisterComponent {
+
+}

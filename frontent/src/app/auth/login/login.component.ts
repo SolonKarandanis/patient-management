9a104d85e916c14faba194304f6b1045bde8c4d7
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-04-30 08:00:11 +0300

    Implementing Auth functionality

---
+++ frontent/src/app/auth/login/login.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  template: `
+    <p>
+      login works!
+    </p>
+  `,
+  styleUrl: './login.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LoginComponent {
+
+}

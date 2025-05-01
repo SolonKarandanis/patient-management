6727f65293982618b6e56d9b03f7b102aec83dd3
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-01 09:42:25 +0300

    Implementing Header

--- frontent/src/app/protected/protected.module.ts
+++ frontent/src/app/protected/protected.module.ts
@@ -4,6 +4,8 @@ import {ProtectedComponent} from './protected.component';
 import {RouterModule} from '@angular/router';
 import {protectedRoutes} from './protected.routes';
 import {SidebarComponent} from '../components/sidebar/sidebar.component';
+import {FooterSmallComponent} from '../components/footer-small/footer-small.component';
+import {HeaderComponent} from '../components/header/header.component';
 
 
 
@@ -13,6 +15,8 @@ import {SidebarComponent} from '../components/sidebar/sidebar.component';
     CommonModule,
     RouterModule.forChild(protectedRoutes),
     SidebarComponent,
+    FooterSmallComponent,
+    HeaderComponent,
   ],
   exports: [RouterModule],
 })

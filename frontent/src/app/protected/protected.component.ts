98a2742ec5e1cbf7e60729483cbbe78aa6211f5d
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-01 09:10:10 +0300

    Creating Protected layouts

--- frontent/src/app/protected/protected.component.ts
+++ frontent/src/app/protected/protected.component.ts
@@ -5,8 +5,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   standalone:false,
   template: `
     <div>
+      <app-sidebar></app-sidebar>
       <div class="relative md:ml-64 bg-blueGray-100">
-
+        <div class="px-4 md:px-10 mx-auto w-full -m-24">
+          <router-outlet></router-outlet>
+        </div>
       </div>
     </div>
   `,

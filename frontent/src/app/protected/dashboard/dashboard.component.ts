c58afb8c343dee67848707048b5bcf2aefdd1a1b
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-03 08:14:18 +0300

    Implementing Layout

--- frontent/src/app/protected/dashboard/dashboard.component.ts
+++ frontent/src/app/protected/dashboard/dashboard.component.ts
@@ -5,7 +5,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   imports: [],
   template: `
     <div class="flex flex-wrap">
-      <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+      <div class="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 text-black">
         dashboard
       </div>
     </div>

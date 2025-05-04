2a18ac32d03dda4a6ead9dca9343739fc020e0cd
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-04 09:20:22 +0300

    API GATEWAY CORS ISSUE

--- frontent/src/environments/environment.ts
+++ frontent/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   webApiEndpointLocal:'http://localhost:4004',
-  backEndDomain:'http://localhost:4004/api',
+  backEndDomain:'http://localhost:4004',
   appVersion: 'v8.0.27',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,

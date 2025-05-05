64df2db73f8dae97c5ff8078d1e5fc1bb9f58199
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-05 08:23:22 +0300

    API GATEWAY CORS ISSUE

--- auth-service/src/main/java/com/pm/authservice/config/authorisation/WebSecurityConfiguration.java
+++ auth-service/src/main/java/com/pm/authservice/config/authorisation/WebSecurityConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -52,7 +53,7 @@ public class WebSecurityConfiguration {
     public SecurityFilterChain configure(HttpSecurity httpSecurity, RequestMatcherBuilder mvc) throws Exception {
 		httpSecurity
 		.csrf(AbstractHttpConfigurer::disable)
-//                .cors(AbstractHttpConfigurer::disable)
+//                .cors(Customizer.withDefaults())
         .cors(c->c.configurationSource(corsConfigurationSource()))
 
         .authorizeHttpRequests(auth-> auth
--- frontent/src/environments/environment.ts
+++ frontent/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  webApiEndpointLocal:'http://localhost:4004',
-  backEndDomain:'http://localhost:4004',
+  webApiEndpointLocal:'http://localhost:4005',
+  backEndDomain:'http://localhost:4005',
   appVersion: 'v8.0.27',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,

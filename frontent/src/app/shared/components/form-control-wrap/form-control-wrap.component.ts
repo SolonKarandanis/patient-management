ab787bfd30cf1796c463304b32bf4a1f7538d79b
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-06 08:12:49 +0300

    Created FormControlWrapComponent

---
+++ frontent/src/app/shared/components/form-control-wrap/form-control-wrap.component.ts
@@ -0,0 +1,33 @@
+import {ChangeDetectionStrategy, Component, input} from '@angular/core';
+import {Skeleton} from 'primeng/skeleton';
+
+@Component({
+  selector: 'app-form-control-wrap',
+  imports: [
+    Skeleton
+  ],
+  template: `
+    @if(!editMode() && !fetchingData()){
+      <div class="min-h-9 p-2 border-2 border-solid rounded-md">
+        {{displayValue()}}
+      </div>
+    }
+    @if(fetchingData()){
+      <p-skeleton height="{{skeletonHeightClass()}}">
+      </p-skeleton>
+    }
+    @if(editMode() && !fetchingData()){
+      <ng-content></ng-content>
+    }
+
+  `,
+  styleUrl: './form-control-wrap.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FormControlWrapComponent {
+
+  editMode = input<boolean>(false);
+  fetchingData = input<boolean>(false);
+  displayValue = input<string>('');
+  skeletonHeightClass = input<string>('2rem');
+}

a2cdce622b4449265b7bb7fcf7d83e59822ea3a6
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-09 07:34:48 +0300

    Created Call State Feature

---
+++ frontent/src/app/core/store/features/call-state.feature.ts
@@ -0,0 +1,34 @@
+import { computed } from '@angular/core';
+import {
+  signalStoreFeature,
+  withComputed,
+  withState,
+} from '@ngrx/signals';
+
+export type CallState = 'init' | 'loading' | 'loaded' | { error: string };
+
+export function withCallState() {
+  return signalStoreFeature(
+    withState<{ callState: CallState }>({ callState: 'init' }),
+    withComputed(({ callState }) => ({
+      loading: computed(() => callState() === 'loading'),
+      loaded: computed(() => callState() === 'loaded'),
+      error: computed(() => {
+        const status = callState();
+        return typeof status === 'object' ? status.error : null;
+      }),
+    }))
+  );
+}
+
+export function setLoading(): { callState: CallState } {
+  return { callState: 'loading' };
+}
+
+export function setLoaded(): { callState: CallState } {
+  return { callState: 'loaded' };
+}
+
+export function setError(error: string): { callState: CallState } {
+  return { callState: { error } };
+}

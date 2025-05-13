78e1384fadac980a5f1b96ed834ade3c713c8bcf
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-13 07:33:19 +0300

    Added route animation

--- frontent/src/app/app.component.ts
+++ frontent/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component, inject, OnInit} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {ActivatedRoute, RouterOutlet} from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import {LanguageService} from '@core/services/language.service';
 import {PrimeNG} from 'primeng/config';
 import {ToastModule} from 'primeng/toast';
 import {LoaderComponent} from '@components/loader/loader.component';
+import {routeTransition} from './route-transition';
 
 @Component({
   selector: 'app-root',
@@ -14,12 +15,14 @@ import {LoaderComponent} from '@components/loader/loader.component';
     LoaderComponent,
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations:[ routeTransition]
 })
 export class AppComponent implements OnInit{
   private readonly translate = inject(TranslateService);
   private readonly languageService = inject(LanguageService);
   private readonly primengConfig = inject(PrimeNG);
+  protected route = inject(ActivatedRoute);
 
   ngOnInit(): void {
     this.translate.setDefaultLang(this.languageService.selectedLanguageIso);

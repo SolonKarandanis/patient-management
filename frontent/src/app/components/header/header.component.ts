88a535a43483256b0adbdbcf4009d08e3e769ee9
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-15 07:24:07 +0300

    Implementing User Dropdown

--- frontent/src/app/components/header/header.component.ts
+++ frontent/src/app/components/header/header.component.ts
@@ -10,29 +10,31 @@ import {UserDropdownComponent} from '../user-dropdown/user-dropdown.component';
     <!-- Navbar -->
     <nav class="relative bg-red-600 md:pt-10 pb-10 pt-10  top-0 left-0 w-full z-10  md:flex-row
         md:flex-nowrap md:justify-start flex items-center p-4">
-      <div class=" w-full mx-auto items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
+      <div class=" w-full mx-auto items-center flex justify-between md:flex-nowrap flex-wrap ">
         <!-- Brand -->
         <a class="text-white text-sm uppercase hidden lg:inline-block font-semibold">
           Dashboard
         </a>
         <!-- Form -->
-        <form class="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3" role="search">
-          <div class="relative flex w-full flex-wrap items-stretch">
-        <span class="z-10 h-full leading-snug font-normal text-center text-blueGray-300
-            absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
-          <i class="pi pi-search"></i>
-        </span>
-            <input type="text"
-              placeholder="Search here..."
-              class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative
+        <div class="flex justify-between gap-5">
+          <form class="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3" role="search">
+            <div class="relative flex w-full flex-wrap items-stretch">
+              <span class="z-10 h-full leading-snug font-normal text-center text-blueGray-300
+                  absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
+                <i class="pi pi-search"></i>
+              </span>
+              <input type="text"
+                     placeholder="Search here..."
+                     class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative
               bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring w-full pl-10"
-            />
-          </div>
-        </form>
-        <!-- User -->
-        <ul class="flex-col md:flex-row list-none items-center hidden md:flex">
-          <app-user-dropdown class="block"></app-user-dropdown>
-        </ul>
+              />
+            </div>
+          </form>
+          <!-- User -->
+          <ul class="flex-col md:flex-row list-none items-center hidden md:flex">
+            <app-user-dropdown ></app-user-dropdown>
+          </ul>
+        </div>
       </div>
     </nav>
   `,
--- frontent/src/app/components/user-dropdown/user-dropdown.component.ts
+++ frontent/src/app/components/user-dropdown/user-dropdown.component.ts
@@ -1,16 +1,51 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 
 @Component({
   selector: 'app-user-dropdown',
   imports: [],
   template: `
-    <p>
-      user-dropdown works!
-    </p>
+    <button
+      id="dropdownUserAvatarButton"
+      data-dropdown-toggle="dropdownAvatar"
+      class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+      type="button"
+      (click)="toggleDropdown()">
+      <span class="sr-only">Open user menu</span>
+      <img class="w-8 h-8 rounded-full" src="assets/img/team-1-800x800.jpg" alt="user photo">
+    </button>
+
+    <!-- Dropdown menu -->
+    <div id="dropdownAvatar"
+         class="z-10 absolute top-16 right-3 bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600"
+         [class.hidden]="dropDownHidden()">
+      <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
+        <div>Solon Karandannis</div>
+        <div class="font-medium truncate">skarandanisgmail.com</div>
+      </div>
+      <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownUserAvatarButton">
+        <li>
+          <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+        </li>
+        <li>
+          <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+        </li>
+        <li>
+          <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+        </li>
+      </ul>
+      <div class="py-2">
+        <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+      </div>
+    </div>
   `,
   styleUrl: './user-dropdown.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserDropdownComponent {
 
+  dropDownHidden = signal(true);
+
+  public toggleDropdown():void{
+    this.dropDownHidden.set(!this.dropDownHidden());
+  }
 }

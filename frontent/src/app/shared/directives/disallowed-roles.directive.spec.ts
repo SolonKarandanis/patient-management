8f658cdcb63905b63ca702339344589d5c17f4dc
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-15 11:28:05 +0300

    Created DisAllowedRolesDirective

---
+++ frontent/src/app/shared/directives/disallowed-roles.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DisallowedRolesDirective } from './disallowed-roles.directive';
+
+describe('DisallowedRolesDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DisallowedRolesDirective();
+    expect(directive).toBeTruthy();
+  });
+});

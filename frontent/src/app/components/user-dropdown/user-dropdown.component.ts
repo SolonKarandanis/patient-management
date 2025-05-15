09b44527044df5612e949b06d9c2b5bac92c4c97
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-15 07:42:24 +0300

    Implemented logout

--- frontent/src/app/components/user-dropdown/user-dropdown.component.ts
+++ frontent/src/app/components/user-dropdown/user-dropdown.component.ts
@@ -38,9 +38,12 @@ import {AuthService} from '@core/services/auth.service';
         </li>
       </ul>
       <div class="py-2">
-        <a href="#"
-           class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
-          out</a>
+        <button
+           class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200
+           dark:hover:text-white"
+            (click)="logout()">
+          Sign out
+        </button>
       </div>
     </div>
   `,

fec52f4f346f872e3c5dfe459b0a3effd170851f
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-20 07:03:13 +0300

    Creating User module

--- frontent/src/app/protected/user/user.routes.ts
+++ frontent/src/app/protected/user/user.routes.ts
@@ -5,16 +5,10 @@ export const userRoutes: Routes =[
     path:'',
     children:[
       {
-        path: 'search',
-        children:[
-          {
-            path: '',
-            loadComponent: () =>
-              import('./search-users/search-users.component').then((m)=>m.SearchUsersComponent),
-          }
-        ]
+        path: '',
+        loadComponent: () =>
+          import('./search-users/search-users.component').then((m)=>m.SearchUsersComponent),
       }
     ]
   },
-  { path: '', redirectTo: 'search' ,pathMatch: 'full',},
 ];

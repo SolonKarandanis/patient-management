77cc824010df5f5c16c3bf09081f65f02d46668f
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-20 07:44:00 +0300

    Implementing Search users form

--- frontent/src/app/protected/user/search-users/search-users.component.ts
+++ frontent/src/app/protected/user/search-users/search-users.component.ts
@@ -13,9 +13,16 @@ import {PageHeaderComponent} from '@components/page-header/page-header.component
       </app-page-header>
       <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
         <form>
-          <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-            User Information
-          </h6>
+          <div class="grid gap-6 mb-6 md:grid-cols-2 mt-4">
+            <div>
+              <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
+              <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
+            </div>
+            <div>
+              <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last name</label>
+              <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Doe" required />
+            </div>
+          </div>
         </form>
       </div>
     </div>

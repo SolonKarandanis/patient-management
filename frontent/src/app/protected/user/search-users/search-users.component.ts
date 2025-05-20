3bba054848c3fb7c345bff34b58362388d0f9678
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-20 07:34:07 +0300

    Implementing Search users

--- frontent/src/app/protected/user/search-users/search-users.component.ts
+++ frontent/src/app/protected/user/search-users/search-users.component.ts
@@ -4,9 +4,20 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'app-search-users',
   imports: [],
   template: `
-    <p>
-      search-users works!
-    </p>
+    <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
+      <div class="rounded-t bg-white mb-0 px-6 py-6">
+        <div class="text-center flex justify-between">
+          <h6 class="text-blueGray-700 text-xl font-bold">My account</h6>
+        </div>
+      </div>
+      <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+        <form>
+          <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
+            User Information
+          </h6>
+        </form>
+      </div>
+    </div>
   `,
   styleUrl: './search-users.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush

1640e0b78235a427c44c85ac9e0f4a65bb45ea7c
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-05-28 06:51:42 +0300

    Created RoleController

---
+++ auth-service/src/main/java/com/pm/authservice/controller/RoleController.java
@@ -0,0 +1,31 @@
+package com.pm.authservice.controller;
+
+import com.pm.authservice.dto.RoleDTO;
+import com.pm.authservice.model.Role;
+import com.pm.authservice.service.RoleService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/roles")
+public class RoleController {
+
+    private final RoleService roleService;
+
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping("/roles")
+    public ResponseEntity<List<RoleDTO>> findAllRoles(){
+        Set<Role> roles = new HashSet<>(roleService.findAll());
+        List<RoleDTO> dtos = roleService.convertToDtoList(roles);
+        return ResponseEntity.ok(dtos);
+    }
+}

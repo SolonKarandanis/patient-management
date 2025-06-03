af235e52f40ee486d0addd703ffc27237253b47d
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-06-03 11:37:30 +0300

    Added animations

---
+++ frontent/src/app/shared/animations/listAnimation.ts
@@ -0,0 +1,21 @@
+import {
+  animate,
+  query,
+  style,
+  transition,
+  trigger,
+  stagger
+} from '@angular/animations';
+
+const listAnimation = trigger('listAnimation', [
+  transition('* <=> *', [
+    query(':enter',
+      [style({ opacity: 0 }), stagger('60ms', animate('600ms ease-out', style({ opacity: 1 })))],
+      { optional: true }
+    ),
+    query(':leave',
+      animate('200ms', style({ opacity: 0 })),
+      { optional: true}
+    )
+  ])
+]);

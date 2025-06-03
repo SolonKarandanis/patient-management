a23fd802b9fb19732155111b6eeafce95d990f4f
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-06-03 08:02:17 +0300

    Search users form

--- frontent/src/app/protected/user/search-users/search-users.component.ts
+++ frontent/src/app/protected/user/search-users/search-users.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, inject, OnInit, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core';
 import {PageHeaderComponent} from '@components/page-header/page-header.component';
 import {BaseComponent} from '@shared/abstract/BaseComponent';
 import {FormBuilder, FormControl, ReactiveFormsModule} from '@angular/forms';

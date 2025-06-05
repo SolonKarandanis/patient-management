04d313621b1b6f563e25e58dc247011d130be2b7
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-06-05 08:25:12 +0300

    Search Users form

---
+++ frontent/src/app/shared/components/saved-searches/saved-searches.component.ts
@@ -0,0 +1,53 @@
+import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core';
+import {Confirmation, ConfirmationService} from 'primeng/api';
+import {SearchService} from '@core/services/search.service';
+import {TranslateService} from '@ngx-translate/core';
+
+
+@Component({
+  selector: 'app-saved-searches',
+  imports: [],
+  template: `
+    <p>
+      saved-searches works!
+    </p>
+  `,
+  styleUrl: './saved-searches.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SavedSearchesComponent implements OnInit{
+  private confirmationService= inject(ConfirmationService);
+  private searchService= inject(SearchService);
+  private translate= inject(TranslateService);
+
+  ngOnInit(): void {
+    this.getSavedSearches();
+  }
+
+  protected handleLoadClick(): void{
+
+  }
+
+  protected handleDeleteClick(): void{
+    // const confirmationConf: Confirmation = {
+    //   message: this.translate.instant('ADVANCED-SEARCH.SAVED-SEARCHES.CONFIRM-DIALOG.message'),
+    //   accept: () => {
+    //     this.toggleLoadingForDeleteBtn();
+    //     const deletedId: number = this.selectedSavedSearch.id!;
+    //
+    //     this.searchService.deleteSavedSearch(deletedId).subscribe({
+    //       next: (savedSearches: SavedSearch[]) => this.afterConfirmDelete(savedSearches),
+    //       error: () => this.toggleLoadingForDeleteBtn(),
+    //     });
+    //   },
+    //   acceptLabel: this.translate.instant('GLOBAL.BUTTONS.yes'),
+    //   rejectLabel: this.translate.instant('GLOBAL.BUTTONS.no'),
+    // };
+    // this.confirmationService.confirm(confirmationConf);
+  }
+
+  private getSavedSearches(): void{
+
+  }
+
+}

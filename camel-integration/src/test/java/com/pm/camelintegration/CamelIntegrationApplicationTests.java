c708d1cfd7bbcec3c3cfe842c0726586f3ba4707
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-06-07 10:12:35 +0300

    Created Camel Integration

---
+++ camel-integration/src/test/java/com/pm/camelintegration/CamelIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.camelintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

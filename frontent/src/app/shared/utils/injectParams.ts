dfa697f1575d87aff56168c4cc67f48f626cf7ab
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-06-10 08:15:31 +0300

    Creating User details component

---
+++ frontent/src/app/shared/utils/injectParams.ts
@@ -0,0 +1,11 @@
+import {assertInInjectionContext, inject, Signal} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {toSignal} from '@angular/core/rxjs-interop';
+import {map} from 'rxjs';
+
+export function injectParams(key?: string): Signal<Params> {
+  assertInInjectionContext(injectParams);
+  const route = inject(ActivatedRoute);
+  const getParam = (params: Params) => key ? params?.[key] ?? null : params;
+  return toSignal(route.params.pipe(map(getParam)), { requireSync: true });
+}

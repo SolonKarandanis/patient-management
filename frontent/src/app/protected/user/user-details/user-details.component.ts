236557611443117e7f835cb03550e0836e1fb197
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-06-11 07:04:05 +0300

    Creating User details component

--- frontent/src/app/protected/user/user-details/user-details.component.ts
+++ frontent/src/app/protected/user/user-details/user-details.component.ts
@@ -1,22 +1,31 @@
 import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core';
 import {PageHeaderComponent} from '@components/page-header/page-header.component';
 import {TranslatePipe} from '@ngx-translate/core';
-import {ActivatedRoute} from '@angular/router';
 import {UserService} from '../data/services/user.service';
 import {USER_DETAILS_PROVIDERS, USERS_DETAILS} from './user-details.provider';
+import {Skeleton} from 'primeng/skeleton';
 
 @Component({
   selector: 'app-user-details',
   imports: [
     PageHeaderComponent,
-    TranslatePipe
+    TranslatePipe,
+    Skeleton
   ],
   template: `
     <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
       <app-page-header>
-        User Details
-<!--        {{ 'USER.SEARCH.title' | translate }}-->
+        {{ 'USER.DETAILS.title' | translate }}
       </app-page-header>
+      @if(vm(); as vm){
+        @defer(when vm.loading){
+          {{ vm.user?.email }}
+        } @placeholder {
+          <p-skeleton
+            width="10rem"
+            height="2rem"/>
+        }
+      }
     </div>
   `,
   styleUrl: './user-details.component.css',
@@ -26,13 +35,12 @@ import {USER_DETAILS_PROVIDERS, USERS_DETAILS} from './user-details.provider';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserDetailsComponent implements OnInit{
-  private activatedRoute = inject(ActivatedRoute);
   private userService = inject(UserService);
-  private user = inject(USERS_DETAILS);
+  protected vm = inject(USERS_DETAILS);
 
 
   ngOnInit(): void {
-   console.log(this.user())
+
   }
 
 }

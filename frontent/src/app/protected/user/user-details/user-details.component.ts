7762898ee229332dd33cd6c3befab30d61f4b67e
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-06-12 08:33:11 +0300

    Creating User details component

--- frontent/src/app/protected/user/user-details/user-details.component.ts
+++ frontent/src/app/protected/user/user-details/user-details.component.ts
@@ -5,11 +5,9 @@ import {UserService} from '../data/services/user.service';
 import {USER_DETAILS_PROVIDER,  USERS_DETAILS} from './user-details.provider';
 import {RequiredFieldsLabelComponent} from '@components/required-fields-label/required-fields-label.component';
 import {BaseComponent} from '@shared/abstract/BaseComponent';
-import {FormErrorComponent} from '@components/form-error/form-error.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {InputText} from 'primeng/inputtext';
-import {FormControlWrapComponent} from '@components/form-control-wrap/form-control-wrap.component';
-import {NgClass} from '@angular/common';
+import {UserDetailsFormComponent} from '../user-details-form/user-details-form.component';
+import {FieldsetComponent} from '@components/fieldset/fieldset.component';
 
 
 @Component({
@@ -18,12 +16,10 @@ import {NgClass} from '@angular/common';
     PageHeaderComponent,
     TranslatePipe,
     RequiredFieldsLabelComponent,
-    FormErrorComponent,
     FormsModule,
-    InputText,
     ReactiveFormsModule,
-    FormControlWrapComponent,
-    NgClass,
+    UserDetailsFormComponent,
+    FieldsetComponent,
 
   ],
   template: `
@@ -34,97 +30,10 @@ import {NgClass} from '@angular/common';
       </app-page-header>
       <app-required-fields-label/>
       @if (vm(); as vm) {
-        @let user = vm.user;
-        <form [formGroup]="form">
-          <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
-            <div class="grid gap-6 mb-6 md:grid-cols-2">
-              <div class="mt-4">
-                <label for="firstName"
-                  [ngClass]="{'app-required-label': form.enabled}">
-                  {{ 'USER.DETAILS.LABELS.firstName' | translate }}
-                </label>
-                <app-form-control-wrap
-                  [editMode]="form.enabled"
-                  [displayValue]="form.get('firstName')?.value"
-                  [fetchingData]="vm.loading">
-                  <input
-                    id="firstName"
-                    pInputText
-                    type="text"
-                    class="border-0 px-3 py-3 !bg-white text-sm shadow w-full !text-black"
-                    formControlName="firstName"
-                    autocomplete="firstName"/>
-                </app-form-control-wrap>
-                <app-form-error
-                  [displayLabels]="isFieldValid('firstName')"
-                  [validationErrors]="form.get('firstName')?.errors"/>
-              </div>
-              <div class="mt-4">
-                <label for="lastName"
-                       [ngClass]="{'app-required-label': form.enabled}">
-                  {{ 'USER.DETAILS.LABELS.lastName' | translate }}
-                </label>
-                <app-form-control-wrap
-                  [editMode]="form.enabled"
-                  [displayValue]="form.get('lastName')?.value"
-                  [fetchingData]="vm.loading">
-                  <input
-                    id="lastName"
-                    pInputText
-                    type="text"
-                    class="border-0 px-3 py-3 !bg-white text-sm shadow w-full !text-black"
-                    formControlName="lastName"
-                    autocomplete="lastName"/>
-                </app-form-control-wrap>
-                <app-form-error
-                  [displayLabels]="isFieldValid('lastName')"
-                  [validationErrors]="form.get('lastName')?.errors"/>
-              </div>
-              <div class="mt-4">
-                <label for="username"
-                       [ngClass]="{'app-required-label': form.enabled}">
-                  {{ 'USER.DETAILS.LABELS.username' | translate }}
-                </label>
-                <app-form-control-wrap
-                  [editMode]="form.enabled"
-                  [displayValue]="form.get('username')?.value"
-                  [fetchingData]="vm.loading">
-                  <input
-                    id="username"
-                    pInputText
-                    type="text"
-                    class="border-0 px-3 py-3 !bg-white text-sm shadow w-full !text-black"
-                    formControlName="username"
-                    autocomplete="username"/>
-                </app-form-control-wrap>
-                <app-form-error
-                  [displayLabels]="isFieldValid('username')"
-                  [validationErrors]="form.get('username')?.errors"/>
-              </div>
-              <div class="mt-4">
-                <label for="username"
-                       [ngClass]="{'app-required-label': form.enabled}">
-                  {{ 'USER.DETAILS.LABELS.email' | translate }}
-                </label>
-                <app-form-control-wrap
-                  [editMode]="form.enabled"
-                  [displayValue]="form.get('email')?.value"
-                  [fetchingData]="vm.loading">
-                  <input
-                    id="username"
-                    pInputText
-                    type="email"
-                    class="border-0 px-3 py-3 !bg-white text-sm shadow w-full !text-black"
-                    formControlName="email"
-                    autocomplete="email"/>
-                </app-form-control-wrap>
-                <app-form-error
-                  [displayLabels]="isFieldValid('email')"
-                  [validationErrors]="form.get('email')?.errors"/>
-              </div>
-            </div>
-          </div>
-        </form>
+        <app-fieldset legend="Details" [toggleable]="false">
+          <app-user-details-form [formGroup]="form" [fetchingData]="vm.loading" />
+        </app-fieldset>
+        <app-user-details-form [formGroup]="form" [fetchingData]="vm.loading" />
       }
     </div>
   `,

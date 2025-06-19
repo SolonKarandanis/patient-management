63a97dfaa9014fb35b04795f5252ffc86c5e7631
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-06-19 07:42:36 +0300

    User edit functionality

---
+++ frontent/src/app/shared/components/fieldset-header-with-buttons/fieldset-header-with-buttons.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FieldsetHeaderWithButtonsComponent } from './fieldset-header-with-buttons.component';
+
+describe('FieldsetHeaderWithButtonsComponent', () => {
+  let component: FieldsetHeaderWithButtonsComponent;
+  let fixture: ComponentFixture<FieldsetHeaderWithButtonsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FieldsetHeaderWithButtonsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FieldsetHeaderWithButtonsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

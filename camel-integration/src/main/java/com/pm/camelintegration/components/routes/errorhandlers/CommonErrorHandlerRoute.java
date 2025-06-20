ab27d34be25acf356d23ce631166aaff3fb6de41
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-06-20 08:24:36 +0300

    Camel errorhandlers

---
+++ camel-integration/src/main/java/com/pm/camelintegration/components/routes/errorhandlers/CommonErrorHandlerRoute.java
@@ -0,0 +1,22 @@
+package com.pm.camelintegration.components.routes.errorhandlers;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.apache.camel.LoggingLevel.WARN;
+
+@Component
+public class CommonErrorHandlerRoute extends RouteBuilder {
+
+    public static final AtomicInteger COUNTER = new AtomicInteger(1);
+
+    @Override
+    public void configure() throws Exception {
+        from("direct:exceptionHandler")
+                .log(WARN, "In Exception Handler")
+                //                .process(e -> SECONDS.sleep(5))
+                .log(WARN, "${body}");
+    }
+}

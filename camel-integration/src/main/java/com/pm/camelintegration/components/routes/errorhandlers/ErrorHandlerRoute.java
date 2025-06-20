4c930c095c7c96a91ee0bf3d2e8e3087912ce403
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-06-20 08:24:36 +0300

    Camel errorhandlers

---
+++ camel-integration/src/main/java/com/pm/camelintegration/components/routes/errorhandlers/ErrorHandlerRoute.java
@@ -0,0 +1,29 @@
+package com.pm.camelintegration.components.routes.errorhandlers;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+import static com.pm.camelintegration.components.routes.errorhandlers.CommonErrorHandlerRoute.COUNTER;
+
+@Component
+public class ErrorHandlerRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        errorHandler(deadLetterChannel("direct:exceptionHandler").maximumRedeliveries(2));
+
+        from("timer:time?period=1000")
+                .process(exchange -> exchange.getIn().setBody(new Date()))
+                .choice()
+                .when(e -> COUNTER.incrementAndGet() % 2 == 0)
+                .bean(HelloBean.class, "callBad")
+                .otherwise()
+                .bean(HelloBean.class, "callGood")
+                .end()
+                .log(LoggingLevel.INFO, ">> ${header.firedTime} >> ${body}")
+                .to("log:reply");
+    }
+}

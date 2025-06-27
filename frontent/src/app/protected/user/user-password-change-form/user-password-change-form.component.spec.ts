ecf90b0cf295c14f6a6eb301b642b91df2566b81
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-06-27 07:26:58 +0300

    Changing password functionality

---
+++ frontent/src/app/protected/user/user-password-change-form/user-password-change-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserPasswordChangeFormComponent } from './user-password-change-form.component';
+
+describe('UserPasswordChangeFormComponent', () => {
+  let component: UserPasswordChangeFormComponent;
+  let fixture: ComponentFixture<UserPasswordChangeFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UserPasswordChangeFormComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(UserPasswordChangeFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

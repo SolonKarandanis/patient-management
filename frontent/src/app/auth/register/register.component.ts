c0dfcc2ff00ca41c7072d1eeb57dc25b5d1efe5d
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-07-01 07:21:48 +0300

    Registering user functionality

--- frontent/src/app/auth/register/register.component.ts
+++ frontent/src/app/auth/register/register.component.ts
@@ -1,10 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, OnInit} from '@angular/core';
 import {SignUpWithComponent} from '../../components/sign-up-with/sign-up-with.component';
+import {TranslatePipe} from '@ngx-translate/core';
+import {BaseComponent} from '@shared/abstract/BaseComponent';
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms';
+import {UserService} from '../../protected/user/data/services/user.service';
+import {FormControlWrapComponent} from '@components/form-control-wrap/form-control-wrap.component';
+import {FormErrorComponent} from '@components/form-error/form-error.component';
+import {InputText} from 'primeng/inputtext';
+import {NgClass} from '@angular/common';
+import {Password} from 'primeng/password';
 
 @Component({
   selector: 'app-register',
   imports: [
-    SignUpWithComponent
+    SignUpWithComponent,
+    TranslatePipe,
+    ReactiveFormsModule,
+    FormControlWrapComponent,
+    FormErrorComponent,
+    InputText,
+    NgClass,
+    Password
   ],
   template: `
     <div class="container mx-auto px-4 h-full">
@@ -16,67 +32,111 @@ import {SignUpWithComponent} from '../../components/sign-up-with/sign-up-with.co
             <app-sign-up-with></app-sign-up-with>
             <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
               <div class="text-blueGray-400 text-center mb-3 font-bold">
-                <small>Or sign up with credentials</small>
+                <small>{{ 'GLOBAL.sign-in-with-credentials' | translate }}</small>
               </div>
-              <form>
-                <div class="relative w-full mb-3">
-                  <label
-                    class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                    htmlFor="grid-password"
-                  >
-                    Name
+              <form [formGroup]="form">
+                <div class="mt-4">
+                  <label for="email"
+                         class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                         [ngClass]="{'app-required-label': form.enabled}">
+                    {{ 'USER.DETAILS.LABELS.email' | translate }}
                   </label>
-                  <input
-                    type="email"
-                    class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm
-                    shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    placeholder="Name"
-                  />
-                </div>
-
-                <div class="relative w-full mb-3">
-                  <label
-                    class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                    htmlFor="grid-password"
-                  >
-                    Email
-                  </label>
-                  <input
-                    type="email"
-                    class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm
-                    shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    placeholder="Email"
-                  />
-                </div>
-                <div class="relative w-full mb-3">
-                  <label
-                    class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                    htmlFor="grid-password"
-                  >
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm
-                    shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                    placeholder="Password"
-                  />
-                </div>
-
-                <div>
-                  <label class="inline-flex items-center cursor-pointer">
+                  <app-form-control-wrap
+                    [editMode]="form.enabled"
+                    [displayValue]="form.get('email')?.value">
                     <input
-                      id="customCheckLogin"
-                      type="checkbox"
-                      class="form-checkbox border-0 text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150 rounded"
-                    />
-                    <span class="ml-2 text-sm font-semibold text-blueGray-600">
-                  I agree with the
-                  <a href="javascript:void(0)" class="text-red-600">
-                    Privacy Policy
-                  </a>
-                </span>
+                      id="email"
+                      pInputText
+                      type="text"
+                      class="border-0 px-3 py-3 !bg-white text-sm shadow w-full !text-black"
+                      formControlName="email"
+                      autocomplete="email"/>
+                  </app-form-control-wrap>
+                  <app-form-error
+                    [displayLabels]="isFieldValid('email')"
+                    [validationErrors]="form.get('email')?.errors"/>
+                </div>
+                <div class="mt-4">
+                  <label for="username"
+                         class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                         [ngClass]="{'app-required-label': form.enabled}">
+                    {{ 'USER.DETAILS.LABELS.username' | translate }}
                   </label>
+                  <app-form-control-wrap
+                    [editMode]="form.enabled"
+                    [displayValue]="form.get('username')?.value">
+                    <input
+                      id="username"
+                      pInputText
+                      type="text"
+                      class="border-0 px-3 py-3 !bg-white text-sm shadow w-full !text-black"
+                      formControlName="username"
+                      autocomplete="username"/>
+                  </app-form-control-wrap>
+                  <app-form-error
+                    [displayLabels]="isFieldValid('username')"
+                    [validationErrors]="form.get('username')?.errors"/>
+                </div>
+                <div class="mt-4">
+                  <label for="firstName"
+                         class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                         [ngClass]="{'app-required-label': form.enabled}">
+                    {{ 'USER.DETAILS.LABELS.firstName' | translate }}
+                  </label>
+                  <app-form-control-wrap
+                    [editMode]="form.enabled"
+                    [displayValue]="form.get('firstName')?.value">
+                    <input
+                      id="firstName"
+                      pInputText
+                      type="text"
+                      class="border-0 px-3 py-3 !bg-white text-sm shadow w-full !text-black"
+                      formControlName="firstName"
+                      autocomplete="firstName"/>
+                  </app-form-control-wrap>
+                  <app-form-error
+                    [displayLabels]="isFieldValid('firstName')"
+                    [validationErrors]="form.get('firstName')?.errors"/>
+                </div>
+                <div class="mt-4">
+                  <label for="lastName"
+                         class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                         [ngClass]="{'app-required-label': form.enabled}">
+                    {{ 'USER.DETAILS.LABELS.lastName' | translate }}
+                  </label>
+                  <app-form-control-wrap
+                    [editMode]="form.enabled"
+                    [displayValue]="form.get('lastName')?.value">
+                    <input
+                      id="lastName"
+                      pInputText
+                      type="text"
+                      class="border-0 px-3 py-3 !bg-white text-sm shadow w-full !text-black"
+                      formControlName="lastName"
+                      autocomplete="lastName"/>
+                  </app-form-control-wrap>
+                  <app-form-error
+                    [displayLabels]="isFieldValid('lastName')"
+                    [validationErrors]="form.get('lastName')?.errors"/>
+                </div>
+                <div class="mt-4">
+                  <label for="password"
+                         class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
+                         [ngClass]="{'app-required-label': form.enabled}">
+                    {{ 'USER.DETAILS.LABELS.password' | translate }}
+                  </label>
+                  <app-form-control-wrap
+                    [editMode]="form.enabled">
+                    <p-password
+                      id="password"
+                      inputStyleClass="border-0 !bg-white text-sm shadow w-full !text-black"
+                      formControlName="password"
+                      [feedback]="true"
+                      [toggleMask]="true" />
+                  </app-form-control-wrap>
+                  <app-form-error
+                    [displayLabels]="isFieldValid('password')"
+                    [validationErrors]="form.get('password')?.errors"/>
                 </div>
                 <div class="text-center mt-6">
                   <button
@@ -97,6 +157,16 @@ import {SignUpWithComponent} from '../../components/sign-up-with/sign-up-with.co
   styleUrl: './register.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class RegisterComponent {
+export class RegisterComponent extends BaseComponent implements OnInit{
+  private fb= inject(FormBuilder);
+  private userService = inject(UserService);
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm():void{
+    this.form= this.userService.initCreateUserForm();
+  }
 
 }

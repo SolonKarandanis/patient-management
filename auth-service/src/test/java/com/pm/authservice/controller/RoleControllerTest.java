27a07107cdbcdfc973ae0385f74039980050d408
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-08-28 13:02:44 +0300

    Implemented RoleControllerTest tests

---
+++ auth-service/src/test/java/com/pm/authservice/controller/RoleControllerTest.java
@@ -0,0 +1,60 @@
+package com.pm.authservice.controller;
+
+import com.pm.authservice.dto.RoleDTO;
+import com.pm.authservice.model.RoleEntity;
+import com.pm.authservice.service.RoleService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import util.TestUtil;
+
+import java.util.HashSet;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@DisplayName("RoleControllerTest")
+@ExtendWith(MockitoExtension.class)
+public class RoleControllerTest {
+
+    @InjectMocks
+    protected RoleController controller;
+
+    @Mock
+    protected RoleService roleService;
+
+    protected RoleEntity role;
+
+    protected RoleDTO roleDto;
+
+    @BeforeEach
+    public void setup(){
+        role = TestUtil.createTestRole();
+        roleDto = TestUtil.createTestRoleDTO();
+    }
+
+    @DisplayName("Fetch all roles")
+    @Test
+    void testFindAllRoles(){
+        List<RoleEntity> roles = List.of(role);
+        List<RoleDTO> roleDTOS = List.of(roleDto);
+        when(roleService.findAll()).thenReturn(roles);
+        when(roleService.convertToDtoList(new HashSet<>(roles))).thenReturn(roleDTOS);
+
+        ResponseEntity<List<RoleDTO>> resp = controller.findAllRoles();
+        assertNotNull(resp);
+        assertNotNull(resp.getBody());
+        assertEquals(resp.getBody(), roleDTOS);
+        assertTrue(resp.getStatusCode().isSameCodeAs(HttpStatus.OK));
+
+        verify(roleService,times(1)).findAll();
+        verify(roleService,times(1)).convertToDtoList(new HashSet<>(roles));
+    }
+}

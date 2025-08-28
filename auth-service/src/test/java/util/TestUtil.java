d25cde3a2ef63ed17566584c6a17936105d00047
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-08-28 13:13:04 +0300

    Implementing UserControllerTest

--- auth-service/src/test/java/util/TestUtil.java
+++ auth-service/src/test/java/util/TestUtil.java
@@ -1,13 +1,15 @@
 package util;
 
-import com.pm.authservice.dto.RoleDTO;
-import com.pm.authservice.dto.UserDTO;
-import com.pm.authservice.dto.UserDetailsDTO;
+import com.pm.authservice.dto.*;
 import com.pm.authservice.model.AccountStatus;
 import com.pm.authservice.model.RoleEntity;
 import com.pm.authservice.model.UserEntity;
 import com.pm.authservice.util.AuthorityConstants;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
@@ -66,6 +68,33 @@ public class TestUtil {
         userDetailsDTO.setEmail(TestConstants.TEST_USER_EMAIL);
         userDetailsDTO.setPublicId(TestConstants.TEST_USER_PUBLIC_ID);
         return userDetailsDTO;
+    }
+
+    public static Authentication getTestAuthenticationFromUserDTO(UserDTO userDto, String token) {
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(userDto, token, Collections.emptyList());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        return SecurityContextHolder.getContext().getAuthentication();
+
+    }
+
+
+    public static Authentication getTestAuthenticationFromUserDTO(UserDTO userDto) {
+        return getTestAuthenticationFromUserDTO(userDto, TestConstants.TEST_TOKEN);
+    }
+
+    public static Paging createPaging(int pagingSize, int pagingIndex, String sortCol, String sortDirection) {
+        Paging output = new Paging();
+        output.setPagingSize(pagingSize);
+        output.setPagingStart(pagingIndex);
+        output.setSortingColumn(sortCol);
+        output.setSortingDirection(sortDirection);
+        return output;
+    }
+
+    public static UsersSearchRequestDTO generateUsersSearchRequestDTO(){
+        UsersSearchRequestDTO dto = new UsersSearchRequestDTO();
+        dto.setPaging(createPaging(10,1,"id","ASC"));
+        return dto;
     }
 
 

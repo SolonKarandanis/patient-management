57e23f564064ef7f92ca4198f031f6388b9f036c
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-09-06 10:22:19 +0300

    Implementing AuthControllerMvcTest

--- auth-service/src/test/java/com/pm/authservice/controller/AuthControllerMvcTest.java
+++ auth-service/src/test/java/com/pm/authservice/controller/AuthControllerMvcTest.java
@@ -1,8 +1,27 @@
 package com.pm.authservice.controller;
 
+import com.pm.authservice.dto.UserDetailsDTO;
+import com.pm.authservice.service.AuthService;
+import com.pm.authservice.service.JwtService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import util.TestConstants;
+import util.TestUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(AuthController.class)
 public class AuthControllerMvcTest {
@@ -10,27 +29,48 @@ public class AuthControllerMvcTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockitoBean
+    protected AuthService authService;
+
+    @MockitoBean
+    protected JwtService jwtService;
+
+    protected final Integer userId = 1;
+    protected Authentication authentication;
+    protected UserDetailsDTO detailsDTO;
+
+    @BeforeEach
+    void setUp() {
+        detailsDTO = TestUtil.createTestUserDetailsDTO(userId);
+        authentication = TestUtil.getTestAuthenticationFromUserDTO(detailsDTO);
+    }
+
+    @DisplayName("Test token validity (Token valid)")
+    @Test
+    void testValidateToken() throws Exception {
+        Map<String, String> map = new HashMap<>();
+        map.put("Authorization", TestConstants.TEST_TOKEN);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setAll(map);
+
+        when(jwtService.extractUsername(anyString())).thenReturn("testuser");
+//        when(jwtService.isTokenValid(anyString(), any(UserDetails.class))).thenReturn(true);
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .get("/auth/validate")
+                                .headers(httpHeaders)
+                )
+                .andExpect(status().is(200))
+                .andReturn();
+
+        verify(jwtService,times(1)).extractUsername(anyString());
+//        verify(jwtService,times(1)).isTokenValid(anyString(), any(UserDetails.class));
+    }
+
 }
 
-//@Test
-//void shouldReturnAdminView() throws Exception {
-//    this.mockMvc
-//            .perform(get("/admin"))
-//            .andExpect(status().is(200))
-//            .andExpect(view().name("admin"))
-//            .andExpect(model().attributeExists("secretMessage"));
-//}
 
 
-//Map<String, String> map = new HashMap<>();
-//        map.put("key", key);
-//        map.put("secret", secret);
-//HttpHeaders httpHeaders = new HttpHeaders();
-//        httpHeaders.setAll(map);
-//MvcResult result = mvc.perform(MockMvcRequestBuilders
-//                .post("/scheduler/enable")
-//                .headers(httpHeaders)
-//                .contentType(MediaType.APPLICATION_JSON_UTF8)
-//                .content(this.getRequstAsJSON()))
-//        .andDo(print())
-//        .andReturn();
+
+

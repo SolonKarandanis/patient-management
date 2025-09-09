d7cdad4a14a80ab6236b9d4b5cfb32bfbbf82c9e
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-09-09 11:36:54 +0300

    Created Credit Card Service module

---
+++ creaditcard-service/src/test/java/com/pm/creaditcardservice/CreaditcardServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.creaditcardservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreaditcardServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

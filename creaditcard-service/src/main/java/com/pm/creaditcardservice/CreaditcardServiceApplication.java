104cf7ec43d30193b370f9dbe7a9c82d0bd3c36f
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-09-09 11:36:54 +0300

    Created Credit Card Service module

---
+++ creaditcard-service/src/main/java/com/pm/creaditcardservice/CreaditcardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.creaditcardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreaditcardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreaditcardServiceApplication.class, args);
+    }
+
+}

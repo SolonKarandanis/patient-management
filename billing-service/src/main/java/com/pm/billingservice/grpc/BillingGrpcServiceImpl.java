5b23c96263ffa35abba51f13a71c30dfdbbf8c16
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-09-30 12:32:24 +0300

    Implementing Billing with spring jdbc

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcServiceImpl.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcServiceImpl.java
@@ -3,15 +3,26 @@ package com.pm.billingservice.grpc;
 import billing.BillingRequest;
 import billing.BillingResponse;
 import billing.BillingServiceGrpc;
+import com.pm.billingservice.model.Billing;
+import com.pm.billingservice.repository.BillingRepository;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.UUID;
+
 @GrpcService
 public class BillingGrpcServiceImpl extends BillingServiceGrpc.BillingServiceImplBase {
     private static final Logger log = LoggerFactory.getLogger(
             BillingGrpcServiceImpl.class);
+
+    private final BillingRepository billingRepository;
+
+    public BillingGrpcServiceImpl(BillingRepository billingRepository) {
+        this.billingRepository = billingRepository;
+    }
+
     @Override
     public void createBillingAccount(BillingRequest billingRequest,
                                      StreamObserver<BillingResponse> responseObserver) {
@@ -19,10 +30,14 @@ public class BillingGrpcServiceImpl extends BillingServiceGrpc.BillingServiceImp
         log.info("createBillingAccount request received {}", billingRequest.toString());
 
         // Business logic - e.g save to database, perform calculates etc
+        Billing billing = new Billing(billingRequest.getPatientId(),billingRequest.getName(),billingRequest.getEmail());
+        billing.setAccountId(UUID.randomUUID());
+        billing.setAccountStatus("ACTIVE");
+        billingRepository.save(billing);
 
         BillingResponse response = BillingResponse.newBuilder()
-                .setAccountId("12345")
-                .setStatus("ACTIVE")
+                .setAccountId(billing.getAccountId().toString())
+                .setStatus(billing.getAccountStatus())
                 .build();
 
         responseObserver.onNext(response);

a3f9301062a8904437498fcca7be47944e5880c4
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-10-15 13:57:14 +0300

    Adding kafka integration tests

--- patient-service/src/main/java/com/pm/patientservice/broker/KafkaProducer.java
+++ patient-service/src/main/java/com/pm/patientservice/broker/KafkaProducer.java
@@ -4,8 +4,11 @@ import com.pm.patientservice.model.Patient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 import patient.events.PatientEvent;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class KafkaProducer implements Producer<Patient> {
@@ -26,7 +29,7 @@ public class KafkaProducer implements Producer<Patient> {
                 .build();
 
         try {
-            kafkaTemplate.send("patient", event.toByteArray());
+            CompletableFuture<SendResult<String, byte[]>> message = kafkaTemplate.send("patient", event.toByteArray());
         } catch (Exception e) {
             log.error("Error sending PatientCreated event: {}", event);
         }

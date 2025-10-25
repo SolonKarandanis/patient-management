a6d78c23d6b923716d1b93ea52f3aebedea6c0a4
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-10-25 14:08:25 +0300

    Implementing AbstractRepository

--- creaditcard-service/src/main/java/com/pm/creaditcardservice/repository/PaymentRepository.java
+++ creaditcard-service/src/main/java/com/pm/creaditcardservice/repository/PaymentRepository.java
@@ -1,9 +1,8 @@
 package com.pm.creaditcardservice.repository;
 
 import com.pm.creaditcardservice.domain.PaymentEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PaymentRepository extends JpaRepository<PaymentEntity,Integer> {
+public class PaymentRepository extends AbstractRepository<PaymentEntity,Integer> {
 }

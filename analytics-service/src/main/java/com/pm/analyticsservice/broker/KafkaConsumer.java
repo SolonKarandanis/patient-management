181a8adb1f9faac63cd12cae3cc21214aac3d236
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-01 12:46:56 +0200

    Added user event types

--- analytics-service/src/main/java/com/pm/analyticsservice/broker/KafkaConsumer.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/broker/KafkaConsumer.java
@@ -6,10 +6,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import patient.events.PatientEvent;
+import user.events.UserEvent;
 
 @Service
 public class KafkaConsumer {
     private static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    private static final String ERROR_DESERIALIZING_EVENT = "Error deserializing event {}";
 
 
     @KafkaListener(topics="patient-events", groupId = "analytics-service")
@@ -18,12 +21,25 @@ public class KafkaConsumer {
             PatientEvent patientEvent = PatientEvent.parseFrom(event);
             // ... perform any business related to analytics here
 
-            log.info("Received Patient Event: [PatientId={},PatientName={},PatientEmail={}]",
+            log.debug("Received Patient Event: [PatientId={},PatientName={},PatientEmail={}]",
                     patientEvent.getPatientId(),
                     patientEvent.getName(),
                     patientEvent.getEmail());
         } catch (InvalidProtocolBufferException e) {
-            log.error("Error deserializing event {}", e.getMessage());
+            log.error(ERROR_DESERIALIZING_EVENT, e.getMessage());
+        }
+    }
+
+    @KafkaListener(topics="user-events", groupId = "analytics-service")
+    public void consumeUserEvent(byte[] event){
+        try {
+            UserEvent userEvent = UserEvent.parseFrom(event);
+            log.debug("Received User Event: [User Id={},Username={},UserEmail={}]",
+                    userEvent.getUserId(),
+                    userEvent.getUsername(),
+                    userEvent.getEmail());
+        } catch (InvalidProtocolBufferException e) {
+            log.error(ERROR_DESERIALIZING_EVENT, e.getMessage());
         }
     }
 }

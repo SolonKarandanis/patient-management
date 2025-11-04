806fbeee98a0a5cdaf8f74f4f7ddb6c5c67b04cd
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-04 08:34:25 +0200

    Created stream-processor

---
+++ stream-processor/src/test/java/com/pm/streamprocessor/StreamProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.streamprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

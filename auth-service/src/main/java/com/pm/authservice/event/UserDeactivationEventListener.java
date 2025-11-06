c47876343a0f1c3b469c70ca3222a1234cc98d60
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-06 13:33:14 +0200

    Implementing notification service

--- auth-service/src/main/java/com/pm/authservice/event/UserDeactivationEventListener.java
+++ auth-service/src/main/java/com/pm/authservice/event/UserDeactivationEventListener.java
@@ -9,21 +9,21 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class UserUpdateEventListener implements ApplicationListener<UserUpdateEvent> {
-    private static final Logger log = LoggerFactory.getLogger(UserUpdateEventListener.class);
+public class UserDeactivationEventListener implements ApplicationListener<UserDeactivationEvent> {
+    private static final Logger log = LoggerFactory.getLogger(UserDeactivationEventListener.class);
 
     private final UserEventService userEventService;
     private final KafkaAnalyticsProducer analyticsProducer;
     private final KafkaNotificationsProducer notificationsProducer;
 
-    public UserUpdateEventListener(UserEventService userEventService, KafkaAnalyticsProducer analyticsProducer, KafkaNotificationsProducer notificationsProducer) {
+    public UserDeactivationEventListener(UserEventService userEventService, KafkaAnalyticsProducer analyticsProducer, KafkaNotificationsProducer notificationsProducer) {
         this.userEventService = userEventService;
         this.analyticsProducer = analyticsProducer;
         this.notificationsProducer = notificationsProducer;
     }
 
     @Override
-    public void onApplicationEvent(UserUpdateEvent event) {
+    public void onApplicationEvent(UserDeactivationEvent event) {
 
     }
 }

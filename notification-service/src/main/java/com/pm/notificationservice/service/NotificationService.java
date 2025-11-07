835e1f4d7e3fb0077d478b23564ffeff441ab76d
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-07 06:40:21 +0200

    Implementing notification service

---
+++ notification-service/src/main/java/com/pm/notificationservice/service/NotificationService.java
@@ -0,0 +1,7 @@
+package com.pm.notificationservice.service;
+
+import notification.events.NotificationEvent;
+
+public interface NotificationService {
+    void sendNotification(NotificationEvent notification);
+}

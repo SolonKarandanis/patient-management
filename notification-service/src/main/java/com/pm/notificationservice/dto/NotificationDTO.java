51ec95543c8885449d06939e0634e9382c400d9f
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-07 06:40:21 +0200

    Implementing notification service

---
+++ notification-service/src/main/java/com/pm/notificationservice/dto/NotificationDTO.java
@@ -0,0 +1,5 @@
+package com.pm.notificationservice.dto;
+
+public record NotificationDTO(String title, String message) {
+
+}
---
+++ notification-service/src/main/java/com/pm/notificationservice/service/NotificationServiceBean.java
@@ -0,0 +1,31 @@
+package com.pm.notificationservice.service;
+
+import com.pm.notificationservice.dto.NotificationDTO;
+import lombok.extern.slf4j.Slf4j;
+import notification.events.NotificationEvent;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class NotificationServiceBean implements NotificationService {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public NotificationServiceBean(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    public void sendNotification(NotificationEvent notificationEvent) {
+        notificationEvent.getUserIdsList().forEach(userId -> {
+            log.info("Sending WS notification to {} with payload {}", userId, notificationEvent.getTitle());
+            NotificationDTO dto = new NotificationDTO(notificationEvent.getTitle(), notificationEvent.getMessage());
+            messagingTemplate.convertAndSendToUser(
+                    userId,
+                    "/notifications",
+                    dto
+            );
+        });
+
+    }
+}

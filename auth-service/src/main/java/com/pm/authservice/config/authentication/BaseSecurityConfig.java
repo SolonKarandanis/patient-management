e44afcfddf578ac659b0fdb591b1e6a652e7669a
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-11 18:40:05 +0200

    Implementing session

--- auth-service/src/main/java/com/pm/authservice/config/authentication/BaseSecurityConfig.java
+++ auth-service/src/main/java/com/pm/authservice/config/authentication/BaseSecurityConfig.java
@@ -5,19 +5,29 @@ import com.pm.authservice.config.authorisation.NoAuthenticationRequestMatcher;
 import com.pm.authservice.model.RoleEntity;
 import com.pm.authservice.repository.RoleRepository;
 import com.pm.authservice.util.SecurityConstants;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 public class BaseSecurityConfig {
+
+    protected static final Logger LOG = LoggerFactory.getLogger(BaseSecurityConfig.class);
 
     @Autowired
     protected RoleRepository roleRepository;
@@ -59,5 +69,24 @@ public class BaseSecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint restAuthenticationEntryPoint() {
+        LOG.info("in WebSecurityConfig.restAuthenticationEntryPoint");
+        return new AuthenticationEntryPoint() {
+
+            @Override
+            public void commence(HttpServletRequest request, HttpServletResponse response,
+                                 AuthenticationException authException) throws IOException, ServletException {
+                LOG.debug("RestAuthenticationEntryPoint called for request uri: {}", request.getRequestURI());
+                // This is invoked when an anonymous user tries to access a
+                // secured REST resource
+                // We should just send a 401 Unauthorized response because there
+                // is no 'login page' to redirect to
+                response.setContentType("application/json");
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            }
+        };
     }
 }
--- auth-service/src/main/java/com/pm/authservice/config/authentication/WebSecurityConfiguration.java
+++ auth-service/src/main/java/com/pm/authservice/config/authentication/WebSecurityConfiguration.java
@@ -8,13 +8,11 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
@@ -47,7 +45,7 @@ public class WebSecurityConfiguration extends BaseSecurityConfig{
         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
         .sessionManagement(session-> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .exceptionHandling(ex -> 
-        	ex.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+        	ex.authenticationEntryPoint(restAuthenticationEntryPoint())
         		.accessDeniedHandler(new CustomAccessDeniedHandler())
         )
                 .authenticationProvider(customAuthProvider);

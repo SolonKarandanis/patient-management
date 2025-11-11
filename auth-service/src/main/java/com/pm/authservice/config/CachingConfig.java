08655f8c647904c8f6d64390f91c3a8e530b4a9c
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-11 12:36:28 +0200

    Fixed bugs in hazelcast

--- auth-service/src/main/java/com/pm/authservice/config/CachingConfig.java
+++ auth-service/src/main/java/com/pm/authservice/config/CachingConfig.java
@@ -1,20 +1,11 @@
 package com.pm.authservice.config;
 
 
-import com.hazelcast.spring.cache.HazelcastCacheManager;
-import com.hazelcast.core.HazelcastInstance;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableCaching
 public class CachingConfig {
 
-    @Bean
-    public CacheManager cacheManager(@Qualifier("hazelcastInstance") HazelcastInstance hazelcastInstance) {
-        return new HazelcastCacheManager(hazelcastInstance);
-    }
 }

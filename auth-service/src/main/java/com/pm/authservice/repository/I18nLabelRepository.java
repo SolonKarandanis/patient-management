dcd6b01d5d88b5a8d1ad9808538f66c83f6643d9
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-12 11:59:58 +0200

    Added I18nLabelRepository

---
+++ auth-service/src/main/java/com/pm/authservice/repository/I18nLabelRepository.java
@@ -0,0 +1,32 @@
+package com.pm.authservice.repository;
+
+
+import com.pm.authservice.model.I18nLabel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface I18nLabelRepository extends JpaRepository<I18nLabel, Integer> {
+
+    @Query(name =I18nLabel.GET_I18N_LABELS)
+    public List<I18nLabel> getI18nLabels();
+
+    @Query(name =I18nLabel.GET_I18N_LABELS_BY_MODULE_ID)
+    public List<I18nLabel> getI18nLabelsByModuleId(@Param("moduleId") Integer moduleId);
+
+    @Query(name =I18nLabel.GET_I18N_LABEL_RESOURCE_KEYS)
+    public List<String> getI18nLabelResourceKeys();
+
+    @Modifying
+    @Query(name =I18nLabel.DELETE_I18N_LABEL_BY_ID)
+    public int deleteI18nLabelById(@Param("labelId") Integer labelId);
+
+    @Modifying
+    @Query(name =I18nLabel.DELETE_I18N_LABELS_WITH_NO_TRANSLATIONS_BY_MODULE_ID)
+    public int deleteI18nLabelsWithNoTranslationsByModuleId(@Param("moduleId") Integer moduleId);
+}

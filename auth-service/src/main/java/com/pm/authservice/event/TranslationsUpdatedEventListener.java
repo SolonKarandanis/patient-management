2d39cf809571ff9b365ae2192c4ffdf74ad4d94f
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-13 18:15:43 +0200

    Implementing Translations Update event

---
+++ auth-service/src/main/java/com/pm/authservice/event/TranslationsUpdatedEventListener.java
@@ -0,0 +1,21 @@
+package com.pm.authservice.event;
+
+import notification.events.NotificationEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TranslationsUpdatedEventListener  extends BaseEventListener implements ApplicationListener<TranslationsUpdatedEvent> {
+    private static final Logger log = LoggerFactory.getLogger(TranslationsUpdatedEventListener.class);
+
+    @Override
+    public void onApplicationEvent(TranslationsUpdatedEvent event) {
+        NotificationEvent notificationEvent = NotificationEvent.newBuilder()
+                .setEventType(EventConstants.I18N_RESOURCES_EDITED)
+                .build();
+        notificationsProducer.sendEvent(notificationEvent);
+        log.info("TranslationsUpdatedEventListener -> onApplicationEvent -> ");
+    }
+}

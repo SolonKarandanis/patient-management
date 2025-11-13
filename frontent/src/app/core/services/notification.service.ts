7a91ed884e8d8037f22cffbbef118b23ef285342
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-13 17:16:31 +0200

    Refactoring Notifications

--- frontent/src/app/core/services/notification.service.ts
+++ frontent/src/app/core/services/notification.service.ts
@@ -1,30 +1,43 @@
 import {inject, Injectable} from '@angular/core';
 import * as Stomp from 'stompjs';
 import SockJS from 'sockjs-client';
-import {Notification} from '@models/notification.model';
+import {NotificationEvent} from '@models/notification.model';
 import {UtilService} from '@core/services/util.service';
 import {WS_BASE_URL} from '@core/token';
+import {AuthService} from '@core/services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationService {
   private stompClient: any;
+  private isConnected = false;
 
-  private utilService = inject(UtilService);
-  private baseUrl = inject(WS_BASE_URL);
+  private readonly utilService = inject(UtilService);
+  private readonly authService = inject(AuthService);
+  private readonly baseUrl = inject(WS_BASE_URL);
 
-
-  public connect(userId: string): void {
+  constructor() {
     const socket = new SockJS(this.baseUrl);
     this.stompClient = Stomp.over(socket);
+  }
 
-    const onConnect = (frame: any) => {
+  public connect(): void {
+    if (this.isConnected) {
+      return;
+    }
+
+    const onConnect = () => {
       // Subscription for actual notifications
-      this.stompClient.subscribe(`/topic/notifications/${userId}`, (message: any) => {
-        const notification = JSON.parse(message.body) as Notification;
-        this.utilService.showMessage("warn",notification.message,notification.title)
-      });
+      const isLoggedIn = this.authService.isLoggedIn();
+      const userId = this.authService.loggedUserId();
+      if(isLoggedIn && userId && !this.isConnected){
+        this.stompClient.subscribe(`/topic/notifications/${userId}`, (message: any) => {
+          const notification = JSON.parse(message.body) as NotificationEvent;
+          this.utilService.showMessage("warn",notification.message,notification.title)
+        });
+      }
+
 
       // Subscription for echo test
       // this.stompClient.subscribe('/topic/greetings', (greeting: any) => {
@@ -42,10 +55,10 @@ export class NotificationService {
     this.stompClient.connect({}, onConnect, onError);
   }
 
-  public disconnect(): void {
+  public disconnect(callback?: () => void): void {
     if (this.stompClient !== null) {
-      this.stompClient.disconnect();
+      this.stompClient.disconnect(callback);
     }
-    console.log("Disconnected");
+    this.isConnected = false;
   }
 }

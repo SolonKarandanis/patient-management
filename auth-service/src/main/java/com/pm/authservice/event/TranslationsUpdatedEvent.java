0b270749aae4b283c21b6352303ef7660092212a
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-13 18:15:43 +0200

    Implementing Translations Update event

---
+++ auth-service/src/main/java/com/pm/authservice/event/TranslationsUpdatedEvent.java
@@ -0,0 +1,14 @@
+package com.pm.authservice.event;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.context.ApplicationEvent;
+
+@Getter
+@Setter
+public class TranslationsUpdatedEvent extends ApplicationEvent {
+
+    public TranslationsUpdatedEvent(Object source) {
+        super(source);
+    }
+}

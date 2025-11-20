797d35694c85faf570acc5f36b299d640bb363a1
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-20 10:20:30 +0200

    Adding Auth stuff

---
+++ auth-service/src/main/java/com/pm/authservice/config/authorisation/RedactedAuthorizationHandler.java
@@ -0,0 +1,15 @@
+package com.pm.authservice.config.authorisation;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.security.authorization.AuthorizationResult;
+import org.springframework.security.authorization.method.MethodAuthorizationDeniedHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedactedAuthorizationHandler implements MethodAuthorizationDeniedHandler {
+
+    @Override
+    public Object handleDeniedInvocation(MethodInvocation methodInvocation, AuthorizationResult authorizationResult) {
+        return "<**********>";
+    }
+}

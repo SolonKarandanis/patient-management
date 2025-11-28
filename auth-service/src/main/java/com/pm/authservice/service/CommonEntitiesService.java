881a32b64cebd1f4be705c6a81de55e589de290e
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-28 10:16:28 +0200

    Implementing resource editing

---
+++ auth-service/src/main/java/com/pm/authservice/service/CommonEntitiesService.java
@@ -0,0 +1,8 @@
+package com.pm.authservice.service;
+
+import com.pm.authservice.dto.ApplicationConfigDTO;
+
+public interface CommonEntitiesService {
+
+    ApplicationConfigDTO getApplicationConfig();
+}

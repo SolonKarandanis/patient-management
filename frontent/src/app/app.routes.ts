c0742fb81101aae7e5ace4d224096f3313072520
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-29 09:29:25 +0200

    Implementing editing resources

--- frontent/src/app/app.routes.ts
+++ frontent/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import {AuthGuard} from '@core/guards/auth.guard';
+import {UnauthorizedComponent} from './unauthorized/unauthorized.component';
 
 export const routes: Routes = [
   {
     path:'auth',
     loadChildren: () =>
       import('./auth/auth.module').then((m)=>m.AuthModule),
+  },
+  {
+    path: 'unauthorized',
+    component: UnauthorizedComponent,
   },
   {
     path: 'error',

10e752dcb95deeff8bf29e5f7e4035a50108842e
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-11-29 10:07:58 +0200

    Implementing editing resources

--- frontent/src/app/protected/i18n/i18n-management/i18n-management.component.ts
+++ frontent/src/app/protected/i18n/i18n-management/i18n-management.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject, OnInit, signal, WritableSignal} from '@angular/core';
+import {I18nTranslationService} from '../data/services/i18n-translation.service';
+import {CommonEntitiesService} from '@core/services/common-entities.service';
+import {BaseComponent} from '@shared/abstract/BaseComponent';
 
 @Component({
   selector: 'app-i18n-management',
@@ -11,6 +14,25 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './i18n-management.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class I18nManagementComponent {
+export class I18nManagementComponent extends BaseComponent implements OnInit {
+  private i18nResourceService = inject(I18nTranslationService);
+  protected commonEntitiesService = inject(CommonEntitiesService);
 
+  protected results = this.i18nResourceService.searchResults;
+  protected totalCount = this.i18nResourceService.totalCount;
+  protected criteriaCollapsed = this.i18nResourceService.criteriaCollapsed;
+  protected tableLoading = this.i18nResourceService.tableLoading;
+  protected loading = this.i18nResourceService.isLoading;
+  protected hasSearched = this.i18nResourceService.hasSearched;
+
+  protected resultsVisible: WritableSignal<boolean> = signal(false);
+  private animationTimer: any;
+
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm(): void{
+    this.form = this.i18nResourceService.initSearchI18nResourceForm();
+  }
 }

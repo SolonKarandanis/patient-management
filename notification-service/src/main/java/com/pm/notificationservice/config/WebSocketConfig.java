bc20c3498f4729658c90a00867a5b517ec07d667
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-12-01 08:26:19 +0200

    Implementing editing resources

--- notification-service/src/main/java/com/pm/notificationservice/config/WebSocketConfig.java
+++ notification-service/src/main/java/com/pm/notificationservice/config/WebSocketConfig.java
@@ -8,6 +8,7 @@ import org.springframework.messaging.converter.DefaultContentTypeResolver;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -41,6 +42,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(1);
+        taskScheduler.setThreadNamePrefix("wss-heartbeat-thread-");
+        taskScheduler.initialize();
+
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableStompBrokerRelay("/topic")
                 .setRelayHost(host)      // Use explicit IPv4 address
@@ -49,8 +55,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                 .setClientPasscode(password)
                 .setSystemLogin(user)    // Credentials for the relay's "system" connection
                 .setSystemPasscode(password)
-                .setSystemHeartbeatSendInterval(10000)
-                .setSystemHeartbeatReceiveInterval(10000);
+                .setTaskScheduler(taskScheduler);
     }
 
     @Override

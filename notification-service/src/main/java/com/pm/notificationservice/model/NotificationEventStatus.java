57c6c06d499f300d95fc5479b7f9168b99fa82d5
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-12-06 10:43:30 +0200

    Created migrations

---
+++ notification-service/src/main/java/com/pm/notificationservice/model/NotificationEventStatus.java
@@ -0,0 +1,8 @@
+package com.pm.notificationservice.model;
+
+public enum NotificationEventStatus {
+    NOTIFICATION_EVENT_CREATED,
+    NOTIFICATION_EVENT_PENDING,
+    NOTIFICATION_EVENT_SENT,
+    NOTIFICATION_EVENT_FAILED
+}

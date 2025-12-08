0cd6b4abd031fcde19cdc6a361a58656d852d7fc
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-12-08 19:39:51 +0200

    Adding monthly patient report

--- analytics-service/src/main/java/com/pm/analyticsservice/AnalyticsServiceApplication.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/AnalyticsServiceApplication.java
@@ -1,9 +1,11 @@
 package com.pm.analyticsservice;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class AnalyticsServiceApplication {
 
     public static void main(String[] args) {
---
+++ analytics-service/src/main/java/com/pm/analyticsservice/config/MonthlyPatientReportIntegrationConfig.java
@@ -0,0 +1,65 @@
+package com.pm.analyticsservice.config;
+
+import lombok.RequiredArgsConstructor;
+import notification.events.NotificationEvent;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.expression.common.LiteralExpression;
+import org.springframework.http.HttpMethod;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.http.inbound.HttpRequestHandlingMessagingGateway;
+import org.springframework.integration.http.inbound.RequestMapping;
+import org.springframework.integration.kafka.outbound.KafkaProducerMessageHandler;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.MessageChannel;
+
+@Configuration
+@RequiredArgsConstructor
+public class MonthlyPatientReportIntegrationConfig {
+
+    private final JobLauncher jobLauncher;
+    private final Job monthlyReportJob;
+    private final KafkaTemplate<String, NotificationEvent> kafkaTemplate;
+
+    @Bean
+    public MessageChannel launchJobChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel notificationChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+
+    public HttpRequestHandlingMessagingGateway httpInboundGateway() {
+        HttpRequestHandlingMessagingGateway gateway = new HttpRequestHandlingMessagingGateway(true);
+        RequestMapping mapping = new RequestMapping();
+        mapping.setPathPatterns("/jobs/start-monthly-report");
+        mapping.setMethods(HttpMethod.POST);
+        gateway.setRequestMapping(mapping);
+        gateway.setRequestChannel(launchJobChannel());
+        return gateway;
+    }
+
+    @ServiceActivator(inputChannel = "launchJobChannel")
+    public void launchJob(String payload) throws Exception {
+        jobLauncher.run(monthlyReportJob, new JobParametersBuilder()
+                .addLong("startTime", System.currentTimeMillis())
+                .toJobParameters());
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "notificationChannel")
+    public KafkaProducerMessageHandler<String, NotificationEvent> kafkaOutboundAdapter() {
+        KafkaProducerMessageHandler<String, NotificationEvent> handler =
+                new KafkaProducerMessageHandler<>(kafkaTemplate);
+        handler.setTopicExpression(new LiteralExpression("notification-events"));
+        return handler;
+    }
+}

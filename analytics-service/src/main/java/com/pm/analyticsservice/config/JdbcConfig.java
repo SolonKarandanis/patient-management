8b3b8b8cd3c459afbe9e2e9e47a0cf6ee877c166
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-12-08 11:51:39 +0200

    Implementing analytics functionality

--- analytics-service/src/main/java/com/pm/analyticsservice/config/JdbcConfig.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/config/JdbcConfig.java
@@ -1,8 +1,11 @@
 package com.pm.analyticsservice.config;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.relational.core.dialect.Dialect;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class JdbcConfig {
@@ -10,5 +13,10 @@ public class JdbcConfig {
     @Bean
     public Dialect clickHouseDialect() {
         return new ClickHouseDialect();
+    }
+    
+    @Bean
+    public DataSource dataSource(DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder().build();
     }
 }

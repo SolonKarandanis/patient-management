96bfc5a799a624d03a58b2415125800561ee209f
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-12-09 20:26:39 +0200

    Added EventProcessingIntegrationConfig

--- analytics-service/src/main/java/com/pm/analyticsservice/config/batch/MonthlyPatientReportIntegrationConfig.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/config/batch/MonthlyPatientReportIntegrationConfig.java
@@ -19,7 +19,6 @@ import org.springframework.integration.kafka.outbound.KafkaProducerMessageHandle
 import org.springframework.kafka.core.KafkaTemplate;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 @Configuration

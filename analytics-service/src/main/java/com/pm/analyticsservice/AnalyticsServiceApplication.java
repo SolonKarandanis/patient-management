c3fa30187f4f1d362a2bb739cd2e7ea34cd9b9b3
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2025-12-09 20:26:39 +0200

    Added EventProcessingIntegrationConfig

--- analytics-service/src/main/java/com/pm/analyticsservice/AnalyticsServiceApplication.java
+++ analytics-service/src/main/java/com/pm/analyticsservice/AnalyticsServiceApplication.java
@@ -3,9 +3,11 @@ package com.pm.analyticsservice;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.integration.config.EnableIntegration;
 
 @SpringBootApplication
 @EnableBatchProcessing
+@EnableIntegration
 public class AnalyticsServiceApplication {
 
     public static void main(String[] args) {

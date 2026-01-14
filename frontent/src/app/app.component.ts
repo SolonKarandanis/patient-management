fc4620f166f027c5007e1f128adbf4b330232d7c
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2026-01-14 12:44:05 +0200

    removed deprecated animations package

--- frontent/src/app/app.component.ts
+++ frontent/src/app/app.component.ts
@@ -1,27 +1,43 @@
-import {Component, effect, inject, OnDestroy, OnInit} from '@angular/core';
-import {ActivatedRoute, RouterOutlet} from '@angular/router';
+import {
+  Component,
+  effect,
+  inject,
+  OnDestroy,
+  OnInit,
+  HostBinding,
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+} from '@angular/core';
+import {
+  ActivatedRoute,
+  RouterOutlet,
+  Router,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError,
+} from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-import {LanguageService} from '@core/services/language.service';
-import {PrimeNG} from 'primeng/config';
-import {ToastModule} from 'primeng/toast';
-import {routeTransition} from './route-transition';
-import {LoaderComponent} from '@components/loader/loader.component';
-import {CommonEntitiesService} from '@core/services/common-entities.service';
-import {NotificationService} from '@core/services/notification.service';
-import {AuthService} from '@core/services/auth.service';
+import { LanguageService } from '@core/services/language.service';
+import { PrimeNG } from 'primeng/config';
+import { ToastModule } from 'primeng/toast';
+import { LoaderComponent } from '@components/loader/loader.component';
+import { CommonEntitiesService } from '@core/services/common-entities.service';
+import { NotificationService } from '@core/services/notification.service';
+import { AuthService } from '@core/services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    ToastModule,
-    LoaderComponent,
-  ],
+  imports: [RouterOutlet, ToastModule, LoaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  animations:[ routeTransition]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit,OnDestroy{
+export class AppComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('outlet')
+  private outlet!: RouterOutlet;
+
   private readonly translate = inject(TranslateService);
   private readonly languageService = inject(LanguageService);
   private readonly commonEntitiesService = inject(CommonEntitiesService);
@@ -29,6 +45,9 @@ export class AppComponent implements OnInit,OnDestroy{
   protected route = inject(ActivatedRoute);
   private readonly notificationService = inject(NotificationService);
   private readonly authService = inject(AuthService);
+  private readonly router = inject(Router);
+  private readonly elementRef = inject(ElementRef);
+  private routerEventsSubscription!: Subscription;
 
   private isLoggedIn = this.authService.isLoggedIn;
   private isWebsocketsEnabled = this.commonEntitiesService.isWebSocketsEnabled;
@@ -58,7 +77,27 @@ export class AppComponent implements OnInit,OnDestroy{
     this.primengConfig.ripple.set(true);
   }
 
+  ngAfterViewInit(): void {
+    this.routerEventsSubscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.elementRef.nativeElement.classList.add('ng-animating');
+        this.elementRef.nativeElement.classList.add('animate-leave');
+      } else if (
+        event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError
+      ) {
+        this.elementRef.nativeElement.classList.remove('animate-leave');
+        this.elementRef.nativeElement.classList.add('animate-enter');
+        setTimeout(() => {
+          this.elementRef.nativeElement.classList.remove('ng-animating');
+          this.elementRef.nativeElement.classList.remove('animate-enter');
+        }, 1000);
+      }
+    });
+  }
+
   ngOnDestroy(): void {
-    // a non-op method to be compliant with the interface
+    this.routerEventsSubscription.unsubscribe();
   }
 }

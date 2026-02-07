71b8b825d3120e8a7b59ba13306ee048fa857e37
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2026-02-07 15:50:19 +0200

    Implementing pie chart

--- frontent/src/app/protected/dashboard/dashboard.component.ts
+++ frontent/src/app/protected/dashboard/dashboard.component.ts
@@ -33,7 +33,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
   constructor() {
     effect(() => {
       this.createEventCountChart(this.patientsDailySummary(), this.patientsChartContainer);
-      this.createEventCountChart(this.userDailySummary(), this.usersChartContainer);
+      this.createUserEventTypePieChart(this.userDailySummary(), this.usersChartContainer);
       this.createPaymentSummaryChart(this.paymentDailySummary(), this.paymentsChartContainer);
     });
   }
@@ -46,8 +46,86 @@ export class DashboardComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.createEventCountChart(this.patientsDailySummary(), this.patientsChartContainer);
-    this.createEventCountChart(this.userDailySummary(), this.usersChartContainer);
+    this.createUserEventTypePieChart(this.userDailySummary(), this.usersChartContainer);
     this.createPaymentSummaryChart(this.paymentDailySummary(), this.paymentsChartContainer);
+  }
+
+  private createUserEventTypePieChart(data: DailyEventCount[], chartContainer: ElementRef): void {
+    if (!data || data.length === 0 || !chartContainer) {
+      return;
+    }
+
+    const element = chartContainer.nativeElement;
+    d3.select(element).select('svg').remove();
+
+    const aggregatedData = d3.rollup(data, v => d3.sum(v, d => d.totalEvents), d => d.eventType);
+    const pieData = Array.from(aggregatedData, ([key, value]) => ({eventType: key, totalEvents: value}));
+
+    const margin = {top: 20, right: 20, bottom: 20, left: 20};
+    const width = 600 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
+    const radius = Math.min(width, height) / 2;
+
+    const svg = d3.select(element).append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${width / 2 + margin.left},${height / 2 + margin.top + 20})`);
+
+    svg.append('text')
+      .attr('x', 0)
+      .attr('y', -height / 2 - margin.top / 2 )
+      .attr('text-anchor', 'middle')
+      .style('font-size', '16px')
+      .style('font-weight', 'bold')
+      .text('Daily User Event Actions');
+
+    const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    const pie = d3.pie<any>().value(d => d.totalEvents);
+    const arc = d3.arc().innerRadius(0).outerRadius(radius);
+
+    const arcs = svg.selectAll('.arc')
+      .data(pie(pieData))
+      .enter().append('g')
+      .attr('class', 'arc');
+
+    arcs.append('path')
+      .attr('d', arc as any)
+      .attr('fill', d => color(d.data.eventType));
+
+
+    const outerArc = d3.arc()
+      .innerRadius(radius * 0.9)
+      .outerRadius(radius * 0.9);
+
+    arcs.append('polyline')
+      .attr('points', function(d: any) {
+        const pos = outerArc.centroid(d);
+        pos[0] = radius * 0.95 * (midAngle(d) < Math.PI ? 1 : -1);
+        return [arc.centroid(d), outerArc.centroid(d), pos] as any;
+      })
+      .style('fill', 'none')
+      .style('stroke', 'black')
+      .style('stroke-width', '1px');
+
+    arcs.append('text')
+      .attr('transform', function(d: any) {
+        const pos = outerArc.centroid(d);
+        pos[0] = radius * (midAngle(d) < Math.PI ? 1 : -1);
+        return `translate(${pos})`;
+      })
+      .attr('dy', '.35em')
+      .style('text-anchor', function(d: any) {
+        return midAngle(d) < Math.PI ? 'start' : 'end';
+      })
+      .style('font-size', '10px')
+      .text(d => d.data.eventType);
+
+    function midAngle(d: any) {
+      return d.startAngle + (d.endAngle - d.startAngle) / 2;
+    }
+
   }
 
   private createEventCountChart(data: DailyEventCount[], chartContainer: ElementRef): void {

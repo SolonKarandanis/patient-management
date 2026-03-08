abbacac3bc46e221bf9198e13d56597160224241
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2026-03-08 09:27:38 +0200

    Added util files

---
+++ notification-service/src/main/java/com/pm/notificationservice/file/common/FileConstants.java
@@ -0,0 +1,16 @@
+package com.pm.notificationservice.file.common;
+
+import org.apache.commons.collections.map.HashedMap;
+
+import java.util.Map;
+
+public class FileConstants {
+    public static final String EMAIL_ATTACHMENT = "EMAIL_ATTACHMENT";
+
+    public static final Map<String,String> fileUsages = new HashedMap();
+
+    static {
+        fileUsages.put("catalogues", "file_reference_id");
+        fileUsages.put("dossiers", "dossiers");
+    }
+}

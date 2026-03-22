83b3f37b9101cf2180b3cf22833e0bf0d1f6d830
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2026-03-22 11:01:59 +0200

    Created fts-service documents

---
+++ fts-service/src/main/java/com/pm/fts/document/AbstractDocument.java
@@ -0,0 +1,35 @@
+package com.pm.fts.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.Setting;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Getter
+@Setter
+@Setting(settingPath = "elasticsearch-settings.json")
+public class AbstractDocument implements Serializable {
+    private static final long serialVersionUID = -6846687739343473149L;
+
+    @Id
+    @Field(type = FieldType.Integer, name = "id")
+    protected Integer id;
+
+    @Field(type = FieldType.Keyword, name = "publicId")
+    protected String publicId;
+
+    protected AbstractDocument(AbstractDocument abs){
+        this.setId(abs.getId());
+        this.setPublicId(abs.getPublicId());
+    }
+}
---
+++ fts-service/src/main/java/com/pm/fts/document/user/UserDocument.java
@@ -0,0 +1,65 @@
+package com.pm.fts.document.user;
+
+import com.pm.fts.document.AbstractDocument;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.elasticsearch.annotations.*;
+
+import java.util.List;
+
+@SuperBuilder
+@Document(indexName = "#{@indexPrefix}auth_service_users", createIndex = true)
+@Getter
+@Setter
+public class UserDocument extends AbstractDocument {
+
+    public UserDocument(AbstractDocument abs) {
+        super(abs);
+    }
+
+    public UserDocument() {
+        super();
+    }
+
+    @Field(type = FieldType.Keyword, name = "username")
+    protected String username;
+
+    @MultiField(mainField = @Field(index = true, type = FieldType.Text, name = "firstName", analyzer = "greek_lowercase_analyzer"),
+            otherFields = {
+                    @InnerField(index = true, type = FieldType.Keyword, suffix = "keyword",  normalizer = "lowercase_normalizer"),
+                    @InnerField(index = true, type = FieldType.Text, suffix = "ngram", analyzer = "ngram_analyzer")
+            })
+    protected String firstName;
+
+    @MultiField(mainField = @Field(index = true, type = FieldType.Text, name = "lastName", analyzer = "greek_lowercase_analyzer"),
+            otherFields = {
+                    @InnerField(index = true, type = FieldType.Keyword, suffix = "keyword",  normalizer = "lowercase_normalizer"),
+                    @InnerField(index = true, type = FieldType.Text, suffix = "ngram", analyzer = "ngram_analyzer")
+            })
+    @Field(type = FieldType.Keyword, name = "lastName")
+    protected String lastName;
+
+    @MultiField(mainField = @Field(index = true, type = FieldType.Text, name = "lastName"),
+            otherFields = {
+                    @InnerField(index = true, type = FieldType.Keyword, suffix = "keyword",  normalizer = "lowercase_normalizer"),
+                    @InnerField(index = true, type = FieldType.Text, suffix = "ngram", analyzer = "ngram_analyzer")
+            })
+    @Field(type = FieldType.Keyword, name = "email")
+    protected String email;
+
+    @Field(type = FieldType.Keyword, name = "status")
+    protected String status;
+
+    @Field(type = FieldType.Boolean, name = "isEnabled")
+    protected Boolean isEnabled;
+
+    @Field(type = FieldType.Boolean, name = "isVerified")
+    protected Boolean isVerified;
+
+    @Field(type = FieldType.Integer, name = "rolesNames")
+    protected List<String> rolesNames;
+
+    @Field(type = FieldType.Integer, name = "roleIds")
+    protected List<Integer> roleIds;
+}

ac4f082d9269c907144928d4532cce246dc3b65b
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2026-03-24 11:39:08 +0200

    Adding dtos

---
+++ auth-service/src/main/java/com/pm/authservice/user/dto/DocumentSearchRequest.java
@@ -0,0 +1,51 @@
+package com.pm.authservice.user.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Getter
+@Setter
+public class DocumentSearchRequest {
+    public enum Type {
+        QUICK,
+        ADVANCED
+    }
+
+    public enum Status {
+        ACTIVE,
+        ALL
+    }
+
+    public enum Role {
+        SA,
+        DOCTOR,
+        PATIENT,
+        GUEST
+    }
+
+    @Valid
+    @NotNull
+    private Type type;
+
+    @Valid
+    private Paging paging;
+
+    @NotNull
+    private Status status;
+
+    @NotNull
+    private Role role;
+
+    @Singular("criterion")
+    @Valid
+    private List<SearchCriterion> criteria;
+
+    private List<String> resultFields;
+}
---
+++ auth-service/src/main/java/com/pm/authservice/user/dto/UserSearchResponseDTO.java
@@ -0,0 +1,18 @@
+package com.pm.authservice.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserSearchResponseDTO {
+    protected List<UserDocumentSearchResultsDTO> content;
+
+    protected Long totalElements;
+}

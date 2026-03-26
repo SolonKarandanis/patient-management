7cf352045bde71de734e34c3b64be0b8f7478806
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2026-03-26 16:12:56 +0200

    Adding repository functionality

--- fts-service/src/main/java/com/pm/fts/repository/user/UserDocumentCustomRepositoryImpl.java
+++ fts-service/src/main/java/com/pm/fts/repository/user/UserDocumentCustomRepositoryImpl.java
@@ -1,10 +1,14 @@
 package com.pm.fts.repository.user;
 
+import com.pm.fts.document.user.UserDocument;
 import com.pm.fts.repository.BaseDocumentCustomRepositoryImpl;
 import com.pm.fts.web.dto.DocumentSearchRequest;
 import com.pm.fts.web.dto.UserSearchResponseDTO;
 import lombok.extern.java.Log;
+import org.springframework.data.elasticsearch.client.elc.NativeQuery;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -27,21 +31,35 @@ public class UserDocumentCustomRepositoryImpl extends BaseDocumentCustomReposito
 
     @Override
     public UserSearchResponseDTO quickSearch(DocumentSearchRequest payload) throws Exception {
-        return null;
+        NativeQuery searchQuery = getNativeQuery(payload,true);
+        // 2. Execute search
+        SearchHits<UserDocument> hits = esOps.search(searchQuery, UserDocument.class, IndexCoordinates.of(getIndex()));
+        log.info("UserDocumentCustomRepositoryImpl -> quickSearch -> hits " + hits.getTotalHits());
+
+        return new UserSearchResponseDTO(hits, hits.getTotalHits());
     }
 
     @Override
     public UserSearchResponseDTO advancedSearch(DocumentSearchRequest payload) throws Exception {
-        return null;
+        NativeQuery searchQuery = getNativeQuery(payload,true);
+        // 2. Execute search
+        SearchHits<UserDocument> hits = esOps.search(searchQuery, UserDocument.class, IndexCoordinates.of(getIndex()));
+        log.info("UserDocumentCustomRepositoryImpl -> advancedSearch-> hits " + hits.getTotalHits());
+        return new UserSearchResponseDTO(hits, hits.getTotalHits());
     }
 
     @Override
     public Long countItems(DocumentSearchRequest payload) throws Exception {
-        return 0L;
+        NativeQuery searchQuery = getNativeQuery(payload,false);
+        return esOps.count(searchQuery, UserDocument.class, IndexCoordinates.of(getIndex()));
     }
 
     @Override
     public UserSearchResponseDTO findActiveItems(DocumentSearchRequest payload) throws Exception {
-        return null;
+        NativeQuery searchQuery = getNativeQuery(payload,false);
+        // 2. Execute search
+        SearchHits<UserDocument> hits = esOps.search(searchQuery, UserDocument.class, IndexCoordinates.of(getIndex()));
+        log.info("UserDocumentCustomRepositoryImpl -> findActiveItems-> hits " + hits.getTotalHits());
+        return new UserSearchResponseDTO(hits, hits.getTotalHits());
     }
 }

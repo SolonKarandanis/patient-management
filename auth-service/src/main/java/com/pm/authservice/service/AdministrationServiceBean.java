e47cd3d443baaa23d8054e65494a56e82e88e5e3
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2026-04-02 19:34:17 +0300

    Implementing re indexing

--- auth-service/src/main/java/com/pm/authservice/service/AdministrationServiceBean.java
+++ auth-service/src/main/java/com/pm/authservice/service/AdministrationServiceBean.java
@@ -2,15 +2,20 @@ package com.pm.authservice.service;
 
 import com.pm.authservice.outbox.service.OutboxService;
 import com.pm.authservice.service.fts.UserFullTextSearchService;
+import com.pm.authservice.user.dto.MinMaxUserIdDTO;
+import com.pm.authservice.user.model.UserEntity;
 import com.pm.authservice.user.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
 
 @Service
 public class AdministrationServiceBean implements AdministrationService{
-
+    private static final int USER_UPDATE_BATCH = 50;
     private static final Logger log = LoggerFactory.getLogger(AdministrationServiceBean.class);
 
     private final UserFullTextSearchService userFullTextSearchService;
@@ -32,8 +37,26 @@ public class AdministrationServiceBean implements AdministrationService{
 
     @Override
     public Boolean triggerAdHocIndexing() {
-
+        log.debug("AdministrationServiceBean --> triggerAdHocIndexing");
+        MinMaxUserIdDTO dto = userService.getMinAndMaxUserId();
+        Integer minUserId = dto.minId();
+        Integer maxUserId = dto.maxId();
+        log.debug("AdministrationServiceBean --> triggerAdHocIndexing --> IdRange=[{}-{}]",minUserId,maxUserId);
+        if(minUserId != null && maxUserId != null){
+            for (int i = minUserId; i <= maxUserId; i += USER_UPDATE_BATCH){
+                int j = Math.min(i + USER_UPDATE_BATCH - 1, maxUserId);
+                log.info(" CASE-Processing: minId={}, maxId={} ", i, j);
+                List<UserEntity> users = userService.findUsersToBeIndexedByIdRange(i,j);
+                if(!CollectionUtils.isEmpty(users)){
+                    initiateReindexing(users);
+                }
+            }
+        }
         return null;
+    }
+
+    protected void initiateReindexing(List<UserEntity> users){
+
     }
 
     @Override

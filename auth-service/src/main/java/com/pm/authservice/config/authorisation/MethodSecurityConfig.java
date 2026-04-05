0c973928f9f85f690ecf73cf707b87d337fe73df
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2026-04-05 09:43:41 +0300

    Refactored code

--- auth-service/src/main/java/com/pm/authservice/config/authorisation/MethodSecurityConfig.java
+++ auth-service/src/main/java/com/pm/authservice/config/authorisation/MethodSecurityConfig.java
@@ -4,11 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.web.context.WebApplicationContext;
 
 @Configuration
 @EnableMethodSecurity
 public class MethodSecurityConfig {
+
+
+    @Bean
+    public GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults("");
+    }
 
 
     @Bean("expressionHandler")

190d73ff7b60733dbc4e0fe705b23acd526c3c94
SolonKarandanis <39560794+SolonKarandanis@users.noreply.github.com>
2026-08-13 09:23:30 +0300

    Refactored WebSocketConfig to use ReactorNettyTcpClient and multiple hosts

--- notification-service/src/main/java/com/pm/notificationservice/config/WebSocketConfig.java
+++ notification-service/src/main/java/com/pm/notificationservice/config/WebSocketConfig.java
@@ -1,18 +1,26 @@
 package com.pm.notificationservice.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.DefaultContentTypeResolver;
 import org.springframework.messaging.converter.JacksonJsonMessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompReactorNettyCodec;
+import org.springframework.messaging.tcp.reactor.ReactorNettyTcpClient;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import reactor.netty.tcp.TcpClient;
 import tools.jackson.databind.ObjectMapper;
 
+import java.net.InetSocketAddress;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
@@ -20,12 +28,13 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    private static final Logger log = LoggerFactory.getLogger(WebSocketConfig.class);
+
     @Value("${artemis.stomp.relay.host}")
-    private String host;
+    private String hosts;
 
     @Value("${artemis.stomp.relay.port}")
     private Integer port;
-
 
     @Value("${spring.artemis.user}")
     private String user;
@@ -41,6 +50,25 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry){
+        List<InetSocketAddress> addresses = Arrays.stream(hosts.split(","))
+                .map(String::trim)
+                .map(h -> new InetSocketAddress(h, port))
+                .toList();
+        log.info("-------> Initializing WebSocketConfig");
+        log.info("-------> STOMP brokers: {}", addresses);
+
+        AtomicInteger idx = new AtomicInteger(0);
+
+        TcpClient tcpClient = TcpClient.create()
+                .remoteAddress(() -> {
+                    InetSocketAddress addr = addresses.get(Math.abs(idx.getAndIncrement()) % addresses.size());
+                    log.info("-------> STOMP relay connecting to {}", addr);
+                    return addr;
+                });
+
+        ReactorNettyTcpClient<byte[]> failoverClient =
+                new ReactorNettyTcpClient<>(tcpClient, new StompReactorNettyCodec());
+
         ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
         taskScheduler.setPoolSize(1);
         taskScheduler.setThreadNamePrefix("wss-heartbeat-thread-");
@@ -48,13 +76,13 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableStompBrokerRelay("/topic")
-                .setRelayHost(host)
-                .setRelayPort(port)
+                .setTcpClient(failoverClient)
                 .setClientLogin(user)
                 .setClientPasscode(password)
                 .setSystemLogin(user)
                 .setSystemPasscode(password)
                 .setTaskScheduler(taskScheduler);
+
     }
 
     @Override
